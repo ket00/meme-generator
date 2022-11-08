6e9eec9ea7dded5116d92dce95fa1d333831041b
ket00 <keti.papidze17@gmail.com>
2022-11-08 16:34:28 +0400

    init

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
     return (
         <header className="header">
-            <img className="header--img" src="./images/Troll face.png" />
+            <img className="header--img" src="./images/Trollface.png" />
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React course-project 3</h4>
         </header>
